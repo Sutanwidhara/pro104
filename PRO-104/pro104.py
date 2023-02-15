7577b426e1c320cfa878807bf5af504fc4082c42
Sutanwidhara <79209127+Sutanwidhara@users.noreply.github.com>
2023-02-15 21:32:20 +0530

    commit1

---
+++ PRO-104/pro104.py
@@ -0,0 +1,13 @@
+import cv2
+img = cv2.imread("solar-system.jpg")
+cv2.putText(img,"Sun",(100,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Mercury",(120,240),cv2.FONT_HERSHEY_COMPLEX,0.4,(255,255,255))
+cv2.putText(img,"Venus",(190,260),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Earth",(290,270),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Mars",(385,250),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Jupiter",(550,370),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Saturn",(790,320),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Uranus",(970,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Neptune",(1115,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.imshow("output",img)
+cv2.waitKey(0)
